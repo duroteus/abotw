2fe814046ab309277842fcf23cf3b2d9b0491905
Gustavo Aragão <gstv.mra@gmail.com>
2024-06-21 16:34:55 +0000

    finaliza o endpoint `api/v1/status`

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -2,13 +2,35 @@ import database from "@/infra/database";
 import { NextRequest } from "next/server";
 
 export const GET = async (req: NextRequest) => {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  const response = new Response(
-    JSON.stringify({ chave: "são acima da média" }),
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult?.rows[0].server_version;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult?.rows[0].max_connections;
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult?.rows[0].count;
+
+  return new Response(
+    JSON.stringify({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: databaseVersionValue,
+          max_connections: +databaseMaxConnectionsValue,
+          opened_connections: databaseOpenedConnectionsValue,
+        },
+      },
+    }),
     {
       status: 200,
     },
   );
-  return response;
 };
