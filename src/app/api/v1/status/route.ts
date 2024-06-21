fb0a98f5852173163ba0a6f932d03fa357e146e2
Gustavo Aragão <gstv.mra@gmail.com>
2024-06-21 12:30:35 +0000

    adiciona `database.ts`

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest } from "next/server";
+import database from "../../../../infra/database";
 
 export const GET = async (req: NextRequest) => {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   const response = new Response(
     JSON.stringify({ chave: "são acima da média" }),
     {
