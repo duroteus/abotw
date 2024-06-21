b97037eae45bfc41228bfdc66bf49d407d8da357
Gustavo Aragão <gstv.mra@gmail.com>
2024-06-21 16:34:19 +0000

    faz o tratamento de erros no `database.js`

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,6 +1,6 @@
 import { Client } from "pg";
 
-async function query(queryObject: string) {
+async function query(queryObject: any) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: +process.env.POSTGRES_PORT!,
@@ -8,11 +8,16 @@ async function query(queryObject: string) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
