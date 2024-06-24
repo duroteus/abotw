46cd9d62d5330005d89b04c927847a67a148ef4c
Gustavo Aragão <gstv.mra@gmail.com>
2024-06-24 14:37:02 +0000

    adiciona a rota `api/v1/migrations` nos métodos GET e POST e seus respectivos testes

---
+++ src/app/api/v1/migrations/route.ts
@@ -0,0 +1,42 @@
+import database from "@/infra/database";
+import { NextRequest } from "next/server";
+import migrationRunner from "node-pg-migrate";
+import { MigrationDirection } from "node-pg-migrate/dist/types";
+import { join } from "node:path";
+
+const getDefaultMigrationOptions = async (dryRun: boolean) => {
+  const dbClient = await database.getNewClient();
+  return {
+    dbClient: dbClient,
+    dryRun: dryRun,
+    dir: join("src", "infra", "migrations"),
+    direction: "up" as MigrationDirection,
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+};
+
+export const GET = async (req: NextRequest) => {
+  const defaultMigrationOptions = await getDefaultMigrationOptions(true);
+  const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+  await defaultMigrationOptions.dbClient.end();
+
+  return new Response(JSON.stringify(pendingMigrations), {
+    status: 200,
+  });
+};
+
+export const POST = async () => {
+  const defaultMigrationOptions = await getDefaultMigrationOptions(false);
+  const migratedMigrations = await migrationRunner({
+    ...defaultMigrationOptions,
+    dryRun: false,
+  });
+  await defaultMigrationOptions.dbClient.end();
+
+  const statusCode = migratedMigrations.length > 0 ? 201 : 200;
+
+  return new Response(JSON.stringify(migratedMigrations), {
+    status: statusCode,
+  });
+};
---
+++ src/tests/integration/api/v1/migrations/post.test.ts
@@ -0,0 +1,53 @@
+import database from "@/infra/database";
+import fs from "fs";
+import path from "path";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  console.log("Database clean");
+  await database.query("drop schema public cascade; create schema public");
+}
+
+describe("POST to /api/v1/migrations", () => {
+  test("should return status code 201", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/migrations", {
+      method: "POST",
+    });
+    const responseBody = await response.json();
+
+    expect(response.status).toBe(201);
+    expect(responseBody.length).toBeGreaterThan(0);
+  });
+
+  test("should return an array", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/migrations", {
+      method: "POST",
+    });
+    const responseBody = await response.json();
+    expect(Array.isArray(responseBody)).toBe(true);
+    expect(responseBody.length).toBe(0);
+    expect(response.status).toBe(200);
+  });
+
+  test("should have the same migrations in the folder and the database", async () => {
+    const migrationsFolder = path.join("src/infra/migrations");
+    const migrationFiles = fs.readdirSync(migrationsFolder);
+
+    const migrationFileNames = migrationFiles.map((file) =>
+      path.basename(file, ".js"),
+    );
+
+    const migrationsResult = await database.query(
+      "SELECT name FROM pgmigrations;",
+    );
+    const migrationNames = migrationsResult.rows.map(
+      (row: { name: string }) => row.name,
+    );
+
+    migrationFileNames.sort();
+    migrationNames.sort();
+
+    expect(migrationFileNames).toEqual(migrationNames);
+  });
+});
