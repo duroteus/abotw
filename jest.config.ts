8cd0fd1db45b44968a0314ee8adddff639f6ced1
Gustavo Aragão <gstv.mra@gmail.com>
2024-06-24 14:35:34 +0000

    adiciona o arquivo de configuração de testes `jest.config.ts`

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import type { Config } from "jest";
+import nextJest from "next/jest";
+dotenv.config({
+  path: ".env.development",
+});
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+const config: Config = {
+  moduleDirectories: ["node_modules"],
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
+};
+
+export default createJestConfig(config);
