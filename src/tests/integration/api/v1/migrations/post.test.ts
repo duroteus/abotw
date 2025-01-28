4a9375350f29940680f4e67782132d9fe874e5b7
Gustavo Aragão <gstv.mra@gmail.com>
2025-01-28 17:41:13 +0000

    test: rename all descriptions to use new pattern

--- src/tests/integration/api/v1/migrations/post.test.ts
+++ src/tests/integration/api/v1/migrations/post.test.ts
@@ -9,44 +9,54 @@ beforeAll(async () => {
 });
 
 describe("POST to /api/v1/migrations", () => {
-  test("should return status code 201", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/migrations", {
-      method: "POST",
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        const responseBody = await response.json();
+
+        expect(response.status).toBe(201);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+
+      test("For the second time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        const responseBody = await response.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBe(0);
+        expect(response.status).toBe(200);
+      });
+
+      test("should have the same migrations in the folder and the database", async () => {
+        const migrationsFolder = path.join("src/infra/migrations");
+        const migrationFiles = fs.readdirSync(migrationsFolder);
+
+        const migrationFileNames = migrationFiles.map((file) =>
+          path.basename(file, ".js"),
+        );
+
+        const migrationsResult = await database.query(
+          "SELECT name FROM pgmigrations;",
+        );
+        const migrationNames = migrationsResult.rows.map(
+          (row: { name: string }) => row.name,
+        );
+
+        migrationFileNames.sort();
+        migrationNames.sort();
+
+        expect(migrationFileNames).toEqual(migrationNames);
+      });
     });
-    const responseBody = await response.json();
-
-    expect(response.status).toBe(201);
-    expect(responseBody.length).toBeGreaterThan(0);
-  });
-
-  test("should return an array", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/migrations", {
-      method: "POST",
-    });
-    const responseBody = await response.json();
-    expect(Array.isArray(responseBody)).toBe(true);
-    expect(responseBody.length).toBe(0);
-    expect(response.status).toBe(200);
-  });
-
-  test("should have the same migrations in the folder and the database", async () => {
-    const migrationsFolder = path.join("src/infra/migrations");
-    const migrationFiles = fs.readdirSync(migrationsFolder);
-
-    const migrationFileNames = migrationFiles.map((file) =>
-      path.basename(file, ".js"),
-    );
-
-    const migrationsResult = await database.query(
-      "SELECT name FROM pgmigrations;",
-    );
-    const migrationNames = migrationsResult.rows.map(
-      (row: { name: string }) => row.name,
-    );
-
-    migrationFileNames.sort();
-    migrationNames.sort();
-
-    expect(migrationFileNames).toEqual(migrationNames);
   });
 });
